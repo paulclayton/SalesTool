be972d13736da81885db5ef1dcc83545dcd50db7
Mike Clayton <mrp_clayton@yahoo.co.uk>
2014-06-20 16:07:25 +0100

    Initial checkin of sales tool.

---
+++ src/ca/demo/salestool/Main.java
@@ -0,0 +1,22 @@
+package ca.demo.salestool;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Salesdata data = new Salesdata();
+		
+		displayGreeting();
+		data.display();
+
+	}
+
+	private static void displayGreeting() {
+		System.out.println("HELLO HAPPY SALES PEOPLE today is the day!");
+		System.out.println("THIS APP SHOWS SALES DATA");
+		
+	}
+
+
+}
+
